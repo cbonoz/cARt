6d02362fa81f4e30a4a75d085290c47609e1c097
cbonoz <chrisdistrict@gmail.com>
2018-10-20 18:51:32 -0700

    reformat home page

--- cart-client/src/components/Home.js
+++ cart-client/src/components/Home.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Slogan from './Slogan';
-import { Button } from 'react-bootstrap'
+import { Button, Grid, Col, Row } from 'react-bootstrap'
 import animationData from './lottie/man_pay.json'
 
-// import logo from '../assets/cart_logo_trans.png'
+import logo from '../assets/cart_horizontal.png'
 import LottieControl from './LottieControl';
 
 class Home extends Component {
@@ -11,28 +11,41 @@ class Home extends Component {
     render() {
         return (
             <div className='home-area'>
-                <LottieControl animationData={animationData}/>
-                <Slogan />
-                <div className='home-main-buttons'>
 
-                <a href="/search">
-                    <Button
-                        className='report-button'
-                        bsStyle="info"
-                        bsSize="large">
-                        Consumers
+                <Row>
+                    <Col md={2} xsHidden></Col>
+                    <Col md={4} xs={12}>
+                        <div className="home-lottie-image">
+                            <LottieControl animationData={animationData} />
+</div>
+                    </Col>
+                    <Col md={6} xs={12}>
+
+                        {/* <img src={logo}/> */}
+                        <h1 className='home-header-text'>cARt</h1>
+                        <Slogan />
+                        <div className='home-main-buttons'>
+                            <a href="/search">
+                                <Button
+                                    className='report-button'
+                                    bsStyle="info"
+                                    bsSize="large">
+                                    Consumers
                     </Button>
-                </a>
-                &nbsp;
+                            </a>
+                            &nbsp;
                 <a href="/report">
-                    <Button
-                        className='report-button'
-                        bsStyle="info"
-                        bsSize="large">
-                        Merchants
+                                <Button
+                                    className='report-button'
+                                    bsStyle="info"
+                                    bsSize="large">
+                                    Merchants
                     </Button>
-                </a>
-                </div>
+                            </a>
+                        </div>
+                    </Col>
+
+                </Row>
             </div>
         );
     }
